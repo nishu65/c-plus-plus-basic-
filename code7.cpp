901968c526ddc9dcb274cc9ff29f078d312d6613
nishu65 <nishubhaiqwerty@gmail.com>
2022-02-23 14:17:30 +0530

    c++ code basic test

---
+++ code7.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int a;
+	cout << "hello there !" << endl;
+	cout << "enter your age = ";
+	cin >> a;
+	if (a >= 18) {
+		cout << "you are adult ." << endl;
+	}
+	else {
+		cout << "you are child motherfucker get lost";
+	}
+	return 0;
+}
